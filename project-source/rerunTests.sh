e25ab7b6c7dd775eae21c6c6f3d03f434e7cd570
JandJB Admin <admin@jandjb.com>
2018-03-28 23:29:03 -0700

    Added script to re-run test suite

---
+++ project-source/rerunTests.sh
@@ -0,0 +1,11 @@
+# -i (interactive)
+# -t Allocate a pseudo-TTY
+
+# find network name using 'docker network ls'
+docker run -it --rm \
+       -v "$(pwd)":/opt/maven \
+       -v "$HOME/.m2":/root/.m2 \
+       -w /opt/maven \
+       --network projectsource_default \
+       maven:3.3.9-jdk-8 \
+       mvn clean test    
